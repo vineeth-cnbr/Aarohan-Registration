053cca6ea70a2efd1c62c638cba901d586b85950
Vineeth Chandran <vineeth.cnbr@gmail.com>
2017-10-08 13:47:52 +0530

    commit

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ admin.initializeApp({
 
 
 
-var config = {
+/*var config = {
     apiKey: "AIzaSyC-Sd37f6e6Ml6ymgy40dgB9y9G3gywu-g",
     authDomain: "aarohan2017-61ac6.firebaseapp.com",
     databaseURL: "https://aarohan2017-61ac6.firebaseio.com/",
     storageBucket: "aarohan2017-61ac6.appspot.com",
-};
+};*/
 
 var schooldetails = {
     schoolname: null,
